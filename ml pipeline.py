63858f21c491c4c8c4ad566816f0ec023b6f2e9a
Aryan bhagat <129328791+bhagataryan@users.noreply.github.com>
2024-12-28 23:12:30 +0530

    Add files via upload

---
+++ ml pipeline.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dataset = pd.read_csv(r"C:\Users\owner's\Downloads\Data.csv")
+
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:,3].values 
+
+
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(strategy="most_frequent")
+
+imputer = imputer.fit(x[:,1:3])
+
+x[:, 1:3] = imputer.transform(x[:,1:3])
+
+
+
+from sklearn.preprocessing import LabelEncoder
+
+labelencoder_x = LabelEncoder() 
+
+labelencoder_x.fit_transform(x[:,0])
+
+x[:,0] = labelencoder_x.fit_transform(x[:,0])
+
+
+
+labelencoder_y = LabelEncoder()
+
+y = labelencoder_y.fit_transform(y)
+
+
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(x, y, test_size = 0.2, random_state = 0 )
